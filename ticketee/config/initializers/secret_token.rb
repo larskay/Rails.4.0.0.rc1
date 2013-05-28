d11ec5d154bec94c774fd63c6d8c195130c87716
Lars Kay <antlak@ANT-PC-LAK.(none)>
2013-05-28 14:35:29 +0200

    New Ticketee App

---
+++ ticketee/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticketee::Application.config.secret_key_base = 'e6a9a780e083824af97e2c25082e8bbc6d6e5716d43b9c3589483754b37aefe54606e2a2c85d9ff2f9eb86767200ae361d9425a2b6cac300585ae25087d9ca97'
